c2f9eda5f1d415ce874c36ec66fc2950b20c7019
Marcin <mm.kodmax@gmail.com>
2023-06-21 21:10:33 +0200

    setValue uSleep

--- src/gpio-output-line.ts
+++ src/gpio-output-line.ts
@@ -4,10 +4,15 @@ import { GPIOException } from './gpio-exception'
 
 export class GPIOOutputLine extends GPIOLineReservation {
 
-    public setValue(value: BitValue): void {
+    /**
+     * Set the value and optionally sleep afterward
+     * @param value 
+     * @param uSleep (optional) number of microseconds to sleep after setting the value 
+     */
+    public setValue(value: BitValue, uSleep?: number): void {
         this.checkReservation()
 
-        if (lineSetValue(this.line, value) === StatusError) {
+        if (lineSetValue(this.line, value, uSleep) === StatusError) {
             throw new GPIOException('Line set value failed')
         }
     }
